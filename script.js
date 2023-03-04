007b95c861367648ca3735910619c4756137b3a7
Girem <iremashvili21@gmail.com>
2023-03-04 19:22:18 +0400

    active status added

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gridContainer = document.getElementById("grid-container");
+const btn = document.querySelector(`.btn`);
 
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
@@ -7,3 +8,29 @@ for (let i = 0; i < 16; i++) {
     gridContainer.appendChild(cell);
   }
 }
+
+gridContainer.addEventListener(
+  "mouseover",
+  (event) => {
+    // highlight the mouseover target
+    console.log(event.target);
+    if (event.target.id == `grid-container`) {
+      return;
+    } else {
+      event.target.classList.add("active");
+    }
+
+    // reset the color after a short delay
+    // setTimeout(() => {
+    //   event.target.style.color = "";
+    // }, 500);
+  },
+  false
+);
+
+function clickPrompt() {
+  let changeGrid = prompt(`enter grid size`);
+  // alert(changeGrid);
+}
+
+btn.addEventListener(`click`, clickPrompt);
